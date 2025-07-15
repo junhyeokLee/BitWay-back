9aa8bec532a8cc9f757b2257872c770badc6c33e
junhyeok <junhyoklee616@gmail.com>
2025-07-15 23:05:21 +0900

    Feat: EnableWebMvc 추가 하니깐 소켓 잘 작동

--- src/main/java/com/example/bitway_back/config/WebSocketConfig.java
+++ src/main/java/com/example/bitway_back/config/WebSocketConfig.java
@@ -2,10 +2,12 @@ package com.example.bitway_back.config;
 
 import com.example.bitway_back.handler.PriceWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocket
+@EnableWebMvc
 public class WebSocketConfig implements WebSocketConfigurer {
 
     private final PriceWebSocketHandler priceWebSocketHandler;
