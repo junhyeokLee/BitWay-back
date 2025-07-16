17caab94b407e5aecac6c0ae790b89c10a0687d1
junhyeok <junhyoklee616@gmail.com>
2025-07-16 23:29:34 +0900

    Fix: healthRouteConfig

--- src/main/java/com/example/bitway_back/controller/HealthCheckController.java
+++ src/main/java/com/example/bitway_back/controller/HealthCheckController.java
@@ -1,12 +1,14 @@
 package com.example.bitway_back.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
-    @GetMapping("/healthz")
-    public String healthz() {
-        return "ok";
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
     }
 }
