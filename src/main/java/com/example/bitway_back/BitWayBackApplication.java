b1fdf4c42d2235c080f76fe734f04415a51e4328
lee junhyeok <junhyeoklee@sports2i.com>
2025-07-18 15:54:49 +0900

    배포 설정

--- src/main/java/com/example/bitway_back/BitWayBackApplication.java
+++ src/main/java/com/example/bitway_back/BitWayBackApplication.java
@@ -2,8 +2,6 @@ package com.example.bitway_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class BitWayBackApplication {
@@ -11,5 +9,4 @@ public class BitWayBackApplication {
     public static void main(String[] args) {
         SpringApplication.run(BitWayBackApplication.class, args);
     }
-
 }
