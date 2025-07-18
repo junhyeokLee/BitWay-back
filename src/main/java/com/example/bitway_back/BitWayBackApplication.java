2258855e95374344c87a15d343cd7a114537ea6b
lee junhyeok <junhyeoklee@sports2i.com>
2025-07-18 12:09:34 +0900

    fix

--- src/main/java/com/example/bitway_back/BitWayBackApplication.java
+++ src/main/java/com/example/bitway_back/BitWayBackApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.bitway_back.repository")
+@EntityScan(basePackages = "com.example.bitway_back.entity")
 public class BitWayBackApplication {
     public static void main(String[] args) {
         SpringApplication.run(BitWayBackApplication.class, args);
