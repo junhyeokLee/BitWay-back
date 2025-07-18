416ff7bf107bb3e41ee8a0d490996972e424c98f
lee junhyeok <junhyeoklee@sports2i.com>
2025-07-18 14:34:10 +0900

    Faivorite service add

--- src/main/java/com/example/bitway_back/BitWayBackApplication.java
+++ src/main/java/com/example/bitway_back/BitWayBackApplication.java
@@ -3,13 +3,15 @@ package com.example.bitway_back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableJpaRepositories(basePackages = "com.example.bitway_back.repository")
+@EntityScan(basePackages = "com.example.bitway_back.domain")
 @SpringBootApplication
 public class BitWayBackApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(BitWayBackApplication.class, args);
     }
+
 }
