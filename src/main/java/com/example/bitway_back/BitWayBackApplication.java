9f68247b4568bf311ab3e5fe04f009d93179ef5f
lee junhyeok <junhyeoklee@sports2i.com>
2025-07-18 14:08:56 +0900

    Faivorite 관련 주석

--- src/main/java/com/example/bitway_back/BitWayBackApplication.java
+++ src/main/java/com/example/bitway_back/BitWayBackApplication.java
@@ -8,9 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.example.bitway_back.repository")
-@EntityScan(basePackages = "com.example.bitway_back.domain")
-@ComponentScan(basePackages = "com.example.bitway_back")
 public class BitWayBackApplication {
     public static void main(String[] args) {
         SpringApplication.run(BitWayBackApplication.class, args);
