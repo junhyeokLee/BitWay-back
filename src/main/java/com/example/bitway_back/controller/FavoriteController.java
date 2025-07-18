e0bd01d93f3317245dac47fe1db60ddcf6e87ac6
lee junhyeok <junhyeoklee@sports2i.com>
2025-07-18 16:48:41 +0900

    favorite coin

--- src/main/java/com/example/bitway_back/controller/FavoriteController.java
+++ src/main/java/com/example/bitway_back/controller/FavoriteController.java
@@ -1,31 +1,31 @@
-//package com.example.bitway_back.controller;
-//import com.example.bitway_back.dto.FavoriteDto;
-//import com.example.bitway_back.domain.FavoriteCoin;
-//import com.example.bitway_back.service.FavoriteService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/favorites")
-//@RequiredArgsConstructor
-//public class FavoriteController {
-//
-//    private final FavoriteService favoriteService;
-//
-//    @GetMapping("/{uuid}")
-//    public List<FavoriteCoin> list(@PathVariable String uuid) {
-//        return favoriteService.getFavorites(uuid);
-//    }
-//
-//    @PostMapping
-//    public FavoriteCoin add(@RequestBody FavoriteDto dto) {
-//        return favoriteService.addFavorite(dto);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public void delete(@PathVariable Long id) {
-//        favoriteService.removeFavorite(id);
-//    }
-//}
+package com.example.bitway_back.controller;
+import com.example.bitway_back.dto.FavoriteDto;
+import com.example.bitway_back.domain.FavoriteCoin;
+import com.example.bitway_back.service.FavoriteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/favorites")
+@RequiredArgsConstructor
+public class FavoriteController {
+
+    private final FavoriteService favoriteService;
+
+    @GetMapping("/{uuid}")
+    public List<FavoriteCoin> list(@PathVariable String uuid) {
+        return favoriteService.getFavorites(uuid);
+    }
+
+    @PostMapping
+    public FavoriteCoin add(@RequestBody FavoriteDto dto) {
+        return favoriteService.addFavorite(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        favoriteService.removeFavorite(id);
+    }
+}
